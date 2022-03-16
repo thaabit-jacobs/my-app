8dd5d1e9f1be5a2d7a5615eddda61bf293c88591
thaabit <mohammedthaabitjacobs@gmail.com>
2022-03-16 12:15:15 +0200

    structure and sttyling complete

--- src/App.js
+++ src/App.js
@@ -3,21 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CURRENCY CONVERTER</h1>
+      <div className="form-container">
+        <div className="input-grp">
+          <div className="input-container">
+            <label for="currencyAmount">Amount</label>
+            <input type="number" id="currencyAmount" name="currencyAmount" min="0"/>
+          </div>
+
+          <div className="input-container">
+            <label for="fromSelector">From</label>
+            <select id="fromSelector" name="fromSelector">
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="ZA">ZA</option>
+                <option value="GBP">GBP</option>
+                <option value="JPY">JPY</option>
+              </select>
+          </div>
+
+          <div className="input-container">
+            <label for="toSelector">To</label>
+            <select id="toSelector" name="toSelector">
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="ZA">ZA</option>
+                <option value="GBP">GBP</option>
+                <option value="JPY">JPY</option>
+              </select>
+          </div>
+        </div>
+
+        <div className="conversion-result-container">
+          <p className="conversion-result"><span className="from">1.00 USD</span> = <span className="to">0.87 EUR</span></p>
+        </div>
+      </div>
     </div>
   );
 }
